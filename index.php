cce4c22d2f3ec9d869334b62451862569e48eca6
codemasterboom <boomhacker@cryptopup.site>
2020-04-01 17:19:00 +0300

    first tests

--- index.php
+++ index.php
@@ -2,21 +2,26 @@
 	try{
 		echo 'Try init core: ';
 		include '../BtcRelax/core.inc';
-		$vCore= \BtcRelax\Core::getIstance(false,true);
-		if ($vCore instanceof \BtcRelax\Core)
-		{
-      echo "Ok";
-      echo "<br>Try init AM: ";
-      $vAM = \BtcRelax\Core::createAM();
-      if ($vAM instanceof \BtcRelax\AM) {
-        echo "Ok";
-      } else { echo "Fail"; }
-      echo "<br>Try init OM: ";
-      $vOM = \BtcRelax\Core::createOM();
-      if ($vOM instanceof \BtcRelax\OM) {
-        echo "Ok";
-      } else { echo "Fail"; }
-    }
+		$vCore=\BtcRelax\Core::getIstance(false,true);
+		if($vCore instanceof \BtcRelax\Core){
+			echo "Ok";
+			echo "<br>Try init AM: ";
+			$vAM=\BtcRelax\Core::createAM();
+			if($vAM instanceof \BtcRelax\AM){
+				echo "Ok";
+			}
+			else{
+				echo "Fail";
+			}
+			echo "<br>Try init OM: ";
+			$vOM=\BtcRelax\Core::createOM();
+			if($vOM instanceof \BtcRelax\OM){
+				echo "Ok";
+			}
+			else{
+				echo "Fail";
+			}
+		}
 	}
 	catch(Exception$e){
 		echo \sptintf("Error:%s",$e->getMessage());
